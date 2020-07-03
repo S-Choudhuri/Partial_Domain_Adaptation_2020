ee6c86feb4904b36adc4efe7cab894f67dbb7ec5
Riti Paul <43425462+RitiP@users.noreply.github.com>
2020-07-03 00:22:42 -0700

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -1,7 +1,23 @@
 import numpy as np
+import torch.optim as optim
 
-def opt_classifier(param):
+def opt_classifier(model, param):
+  
+  config = {'lr': param['lr_classifier'], 'betas': (param["b1_classifier"], param["b2_classifier"])}
+  opt = optim.Adam(model.parameters(), **config)
+  
+  return opt
 
-def opt_discriminator(param):
+def opt_discriminator(model, param):
+  
+  config = {'lr': param['lr_discriminator'], 'betas': (param["b1_discriminator"], param["b2_discriminator"])}
+  opt = optim.Adam(model.parameters(), **config)
+  
+  return opt
 
-def opt_combined(param):
+def opt_combined(model, param):
+  
+  config = {'lr': param['lr_combined'], 'betas': (param["b1_combined"], param["b2_combined"])}
+  opt = optim.Adam(model.parameters(), **config)
+  
+  return opt
