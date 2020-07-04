2188c06334d7e6bcfcfc66bd27e30edff06c4a58
Riti Paul <43425462+RitiP@users.noreply.github.com>
2020-07-04 15:08:15 -0700

    Update driver.py
    Adding training structure

--- driver.py
+++ driver.py
@@ -5,6 +5,15 @@ import argparse
 import random
 import numpy as np
 from tensorflow import set_random_seed
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+from torchvision import datasets
+from torchvision import transforms
+from optimizer import *
+from data_loader import *
+from loss_weights import *
 
 os.environ['PYTHONHASHSEED']=str(SEED)
 np.random.seed(SEED)
@@ -14,9 +23,6 @@ random.seed(SEED)
 from PIL import Image
 from keras.utils import to_categorical
 from sklearn.metrics import accuracy_score
-import model
-import optimizer
-import loss_weights
 
 def pil_loader(path):
     # Return the RGB variant of input image
@@ -63,8 +69,171 @@ def batch_generator(data, batch_size):
         mini_batch_indices = np.random.choice(all_examples_indices, size = batch_size, replace = False)
         tbr = [k[mini_batch_indices] for k in data]
         yield tbr
+        
+def exp_lr_scheduler(optimizer, step, init_lr=lr, lr_decay_step=lr_decay_step, step_decay_weight=step_decay_weight):
+
+    # Decay learning rate by a factor of step_decay_weight every lr_decay_step
+    current_lr = init_lr * (step_decay_weight ** (step / lr_decay_step))
+
+    if step % lr_decay_step == 0:
+        print 'learning rate is set to %f' % current_lr
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = current_lr
+
+    return optimizer
 
 def train(param):
+    
+    #define dataloaders for source and target
+	dataset_source = DataLoader(param['source_path'], param['inp_dims'])
+	dataloader_source = torch.utils.data.DataLoader(dataset=dataset_source, batch_size=batch_size, shuffle=True, num_workers=4)
+
+	dataset_target = DataLoader(param['target_path'], param['inp_dims'])
+	dataloader_target = torch.utils.data.DataLoader(dataset=dataset_target, batch_size=batch_size, shuffle=True, num_workers=4)
+
+	len_dataloader = min(len(dataloader_source), len(dataloader_target))
+    
+    #######################
+	# define loss         #
+	#######################
+	#target/source shared and target/source private
+	loss_diff = DiffLoss()
+
+	#classifier loss
+	loss_classification = torch.nn.CrossEntropyLoss()
+
+	#reconstruction losses
+	loss_recon1 = MSE()
+	loss_recon2 = SIMSE()
+
+	#discriminator adversarial loss
+	loss_adv = WLoss()
+
+	#Define models
+	classifier = Classifier()
+
+	discriminator = Discriminator()
+
+	combined_model = Model()
+
+	#####################
+	# setup optimizer   #
+	#####################
+	opt_model = opt_combined(combined_model, param)
+	opt_discriminator = opt_discriminator(discriminator, param)
+	opt_classifier = opt_classifier(classifier, param)
+
+    for epoch in range(param['num_iterations']):
+        data_source_iter = iter(dataloader_source)
+    	data_target_iter = iter(dataloader_target)
+        
+        i = 0
+        
+        while i < len_dataloader:
+            
+            #target training
+    		data_target = data_target_iter.next()
+    		t_img, t_label = data_target
+
+    		combined_model.zero_grad()
+    		classifier.zero_grad()
+    		discriminator.zero_grad()
+
+    		loss = 0
+    		batch_size = len(t_label)
+
+    		input_img = torch.FloatTensor(batch_size, 3, image_size, image_size)
+        	class_label = torch.LongTensor(batch_size)
+        	domain_label = torch.ones(batch_size)
+        	domain_label = domain_label.long()
+
+        	input_img.resize_as_(t_img).copy_(t_img)
+        	class_label.resize_as_(t_label).copy_(t_label)
+        	target_inputv_img = Variable(input_img)
+        	#target_classv_label = Variable(class_label)
+        	target_domainv_label = Variable(domain_label)
+
+        	target_private, target_shared, target_rec = combined_model(target_inputv_img, mode='target')
+        	target_domain_label = discriminator(target_shared)
+
+        	#target loss calculation
+        	target_domain = loss_adv(target_domain_label, target_domainv_label)
+        	loss += target_domain
+
+        	target_diff = loss_diff(target_private, target_shared)
+        	loss += target_diff
+
+        	target_mse = loss_recon1(target_rec, target_inputv_img)
+        	loss += target_mse
+
+        	target_simse = loss_recon2(target_rec, target_inputv_img)
+        	loss += target_simse
+
+        	loss.backward()
+        	opt_model.step()
+        	opt_discriminator.step()
+
+
+    		#source training
+    		data_source = data_source_iter.next()
+        	s_img, s_label = data_source
+
+        	combined_model.zero_grad()
+    		classifier.zero_grad()
+    		discriminator.zero_grad()
+
+    		loss = 0
+    		batch_size = len(s_label)
+
+    		input_img = torch.FloatTensor(batch_size, 3, image_size, image_size)
+        	class_label = torch.LongTensor(batch_size)
+        	domain_label = torch.zeros(batch_size)
+        	domain_label = domain_label.long()
+
+    		input_img.resize_as_(input_img).copy_(s_img)
+        	class_label.resize_as_(s_label).copy_(s_label)
+        	source_inputv_img = Variable(input_img)
+        	source_classv_label = Variable(class_label)
+        	source_domainv_label = Variable(domain_label)
+
+        	source_private, source_shared, source_rec = combined_model(source_inputv_img, mode='source')
+        	source_domain_label = discriminator(source_shared)
+        	source_class_label = classifier(source_shared)
+
+    		#source loss calculation
+    		source_domain = loss_adv(source_domain_label, source_domainv_label)
+    		loss += source_domain
+
+    		source_class = loss_classification(source_class_label, source_classv_label)
+    		loss += source_class
+
+    		source_diff = loss_diff(source_private, source_shared)
+    		loss += source_diff
+
+    		source_mse = loss_recon1(source_rec, source_inputv_img)
+    		loss += source_mse
+
+    		source_simse = loss_recon2(source_rec, source_inputv_img)
+    		loss += source_simse
+
+    		loss.backward()
+    		# if needed to update the lr, uncomment the following lines
+    		# opt_model = exp_lr_scheduler(optimizer=opt_model, step=i)
+    		# opt_discriminator = exp_lr_scheduler(optimizer=opt_discriminator, step=i)
+    		# opt_classifier = exp_lr_scheduler(optimizer=opt_classifier, step=i)
+    		opt_model.step()
+        	opt_discriminator.step()
+        	opt_classifier.step()
+
+        	i += 1
+            
+        #print loss for each epoch
+
+        #save the model per 50 epochs
+        if epoch % 50 == 0:
+        	torch.save(combined_model.state_dict(), param['output_dir'] + '/pda_epoch_' + str(epoch) + '.pth')
+    
 
 if __name__ == "__main__":
     # Read parameter values from the console
