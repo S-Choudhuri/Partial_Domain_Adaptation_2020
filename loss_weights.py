a0637bb84447c55ff85a12a3ce7ef83fc2254dfb
Riti Paul <43425462+RitiP@users.noreply.github.com>
2020-08-21 15:34:41 -0700

    Add simse, diff loss

--- loss_weights.py
+++ loss_weights.py
@@ -20,3 +20,33 @@ def loss_weight(yt, ysb):
     w_sample_adv[0:ysb.shape[0]] = w_class[ysbi]
 
     return w_sample_class, w_sample_adv
+
+def MSE(pred, real):
+    diffs = torch.add(real, -pred)
+    n = torch.numel(diffs.data)
+    mse = torch.sum(diffs.pow(2)) / n
+    
+    return mse
+
+def SIMSE(pred, real):
+    diffs = torch.add(real, - pred)
+    n = torch.numel(diffs.data)
+    simse = torch.sum(diffs).pow(2) / (n ** 2)
+    
+    return simse
+
+def DiffLoss(x, recon_x):
+    
+    batch_size = x.size(0)
+    x = x.view(batch_size, -1)
+    recon_x = recon_x.view(batch_size, -1)
+    
+    x_l2_norm = torch.norm(x, p=2, dim=1, keepdim=True).detach()
+    x_l2 = x.div(x_l2_norm.expand_as(x) + 1e-6)
+    
+    recon_x_l2_norm = torch.norm(recon_x, p=2, dim=1, keepdim=True).detach()
+    recon_x_l2 = recon_x.div(recon_x_l2_norm.expand_as(recon_x) + 1e-6)
+    
+    diff_loss = torch.mean((x_l2.t().mm(recon_x_l2)).pow(2))
+    
+    return diff_loss
