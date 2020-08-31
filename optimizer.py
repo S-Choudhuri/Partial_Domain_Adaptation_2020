1c78fc91b2c6a362644f6e958f649202314599d8
Sandipan Choudhuri <schoud13@asu.edu>
2020-08-31 01:39:41 -0700

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -1,23 +1,5 @@
-import numpy as np
 import torch.optim as optim
 
-def opt_classifier(model, param):
-  
-  config = {'lr': param['lr_classifier'], 'betas': (param["b1_classifier"], param["b2_classifier"])}
-  opt = optim.Adam(model.parameters(), **config)
-  
-  return opt
+def opt_(net_params, lr, momentum, weight_decay):
+    return optim.SGD(net_params, lr=lr, momentum=momentum, weight_decay=weight_decay)
 
-def opt_discriminator(model, param):
-  
-  config = {'lr': param['lr_discriminator'], 'betas': (param["b1_discriminator"], param["b2_discriminator"])}
-  opt = optim.Adam(model.parameters(), **config)
-  
-  return opt
-
-def opt_combined(model, param):
-  
-  config = {'lr': param['lr_combined'], 'betas': (param["b1_combined"], param["b2_combined"])}
-  opt = optim.Adam(model.parameters(), **config)
-  
-  return opt
